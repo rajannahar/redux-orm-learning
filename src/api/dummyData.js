7416650ba28122a694ad88f3bc22e84da19688df
rajannahar <R_N101@msn.com>
2017-04-03 16:39:05 +0100

    first commit

---
+++ src/api/dummyData.js
@@ -0,0 +1,79 @@
+const data = {
+    "books":[
+
+      {
+        "id" : 1,
+        "title": "Harry Potter",
+        "authors" : [
+          {
+            "id" : 1,
+            "name" : "Hagrid"
+          }, {
+            "id" : 2,
+            "name" : "Dobby"
+          }
+        ],
+        "publisher" : {
+          "id" : 1,
+          "name" : "Hogwarts Ltd"
+        }
+      },
+
+      {
+        "id" : 2,
+        "title": "Lord of the rings",
+        "authors" : [
+          {
+            "id" : 3,
+            "name" : "Merry"
+          }, {
+            "id" : 4,
+            "name" : "Pippin"
+          }
+        ],
+        "publisher" : {
+          "id" : 2,
+          "name" : "Gandalf Inc"
+        }
+      },
+
+      {
+        "id" : 3,
+        "title": "Lord of Hogwarts",
+        "authors" : [
+          {
+            "id" : 2,
+            "name" : "Dobby"
+          }, {
+            "id" : 4,
+            "name" : "Pippin"
+          }
+        ],
+        "publisher" : {
+          "id" : 2,
+          "name" : "Gandalf Inc"
+        }
+      }, 
+
+      {
+        "id" : 4,
+        "title": "Hobbit gang",
+        "authors" : [
+          {
+            "id" : 2,
+            "name" : "Dobby"
+          }, {
+            "id" : 5,
+            "name" : "Gimli"
+          }
+        ],
+        "publisher" : {
+          "id" : 3,
+          "name" : "Orc Squad"
+        }
+      }
+      
+  ]
+}
+
+export default data;
